c6bb2c60661388971254319f2268ef841abb06dc
Osada Vidath <osadavidath@gmail.com>
2025-09-21 02:01:18 +0530

    feat: changed header style

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
-const dropdownItem = document.querySelector(".header__nav-item--dropdown");
-const dropdownToggle = document.querySelector(".header__nav-link--dropdown");
-
-if (dropdownItem && dropdownToggle) {
-  dropdownToggle.addEventListener("click", (e) => {
-    e.preventDefault();
-    dropdownItem.classList.toggle("header__nav-item--active");
-  });
-
-  document.addEventListener("click", (e) => {
-    if (!dropdownItem.contains(e.target)) {
-      dropdownItem.classList.remove("header__nav-item--active");
-    }
-  });
-
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-      dropdownItem.classList.remove("header__nav-item--active");
-    }
-  });
-}
-
 const newsletterForm = document.querySelector(".footer__form");
 
 if (newsletterForm) {
